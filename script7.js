6c184f1c4fd7c0897aaf5d46bc814cd811d8245a
Muhammad Anas <159054672+muhammadanas05@users.noreply.github.com>
2024-04-23 13:42:34 +0500

    Add files via upload

---
+++ script7.js
@@ -0,0 +1,66 @@
+function clr(a)
+      {
+        switch(a)
+        {
+          case 1:
+          document.getElementById('iph').src = "11pro1.png";
+            break;
+          case  2:
+            document.getElementById('iph').src = "img8r.png";
+            document.getElementById('iph').width = "586";
+            document.getElementById('iph').height = "630";
+            break;
+        }
+      }
+      function dropdown()
+      {
+        switch(document.getElementById('cnty').value)
+        {
+          case "2":
+          document.getElementById('city7').selected = true;
+          document.getElementById('city2').hidden = false;
+          document.getElementById('city3').hidden = false;
+          document.getElementById('city6').hidden = true;
+          document.getElementById('city4').hidden = true;
+          document.getElementById('city5').hidden = true;
+          break;
+          case "3":
+          document.getElementById('city7').selected = true;
+          document.getElementById('city4').hidden = false;
+          document.getElementById('city5').hidden = false;
+          document.getElementById('city6').hidden = true;
+          document.getElementById('city2').hidden = true;
+          document.getElementById('city3').hidden = true;
+          break;
+        }
+      }
+      function pay()
+      {
+        switch(document.getElementById('paym').value)
+        {
+          case "2":
+            document.getElementById('ccard1').hidden = true;
+            document.getElementById('ccard2').hidden = true;
+            document.getElementById('ccard3').hidden = true;
+            document.getElementById('ccard4').hidden = true;
+            document.getElementById('ccard5').hidden = true;
+            document.getElementById('card1').required = false;
+            document.getElementById('card2').required = false;
+            document.getElementById('card3').required = false;
+            document.getElementById('card4').required = false;
+            document.getElementById('card5').required = false;
+            break;
+          case "3":
+            document.getElementById('ccard1').hidden = false;
+            document.getElementById('ccard2').hidden = false;
+            document.getElementById('ccard3').hidden = false;
+            document.getElementById('ccard4').hidden = false;
+            document.getElementById('ccard5').hidden = false;
+            document.getElementById('card1').required = true;
+            document.getElementById('card2').required = true;
+            document.getElementById('card3').required = true;
+            document.getElementById('card4').required = true;
+            document.getElementById('card5').required = true;
+            break;
+        }
+      }
